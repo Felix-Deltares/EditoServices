1a45a90fea261c4468b4fc74648d9c5e5b313211
Felix-Deltares <131261890+Felix-Deltares@users.noreply.github.com>
2025-12-16 14:05:42 +0100

    Initialize conda environment setup in init.sh
    Added script to update and activate conda environment.

--- 4dvarnet-relocatable/init.sh
+++ 4dvarnet-relocatable/init.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+# kill process if anything fails
+set -e
+
+ENV_NAME=4dvarnet-relocatable-model
+ENV_FILE=https://github.com/Deltares-research/EditoServices/blob/main/4dvarnet-relocatable/environment_reduced.yaml
+
+echo "▶ Updating conda environment from ${ENV_FILE}"
+conda env update -f ${ENV_FILE}
+
+echo "▶ Activating environment ${ENV_NAME}"
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate ${ENV_NAME}
+
+# clear output
+# jupyter nbconvert --clear-output --inplace modelbuilder_example.ipynb
+# extend modelbuilder notebook, creates modelbuilder_example_edito.ipynb
+# wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/modelbuilder/extend_modelbuilder_notebook.py
+# python extend_modelbuilder_notebook.py
+# remove redundant files
+# rm modelbuilder_example.ipynb
+# rm extend_modelbuilder_notebook.py
