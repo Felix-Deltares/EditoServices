b886a1184f77b233188e6019eeddc52a8c7770c2
Felix-Deltares <131261890+Felix-Deltares@users.noreply.github.com>
2025-12-17 10:20:58 +0100

    Update init.sh to set up conda environment

--- 4dvarnet-relocatable/init.sh
+++ 4dvarnet-relocatable/init.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# kill process if anything fails
+set -e
+
+$EnvName = "jupyter_env"
+$EnvFile = "https://github.com/Deltares-research/EditoServices/blob/main/4dvarnet-relocatable/environment_reduced.yaml"
+
+#Write-Host "Updating conda environment from $EnvFile"
+conda env update -f $EnvFile
+
+Write-Host "Activating environment $EnvName"
+conda activate $EnvName
+
+wget https://raw.githubusercontent.com/Deltares-research/EditoServices/main/4dvarnet-relocatable/4DVarNet_NorthSea_workflow.ipynb
